96d6fb7abd5f202de1372a118026172612779804
Shin Sungyun <sinsungyun1630@gmail.com>
2023-07-16 20:36:57 +0900

    pm2 restart all

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -1,4 +1,5 @@
-# REPOSITORY=/home/ubuntu/escapediary-BE-test
-# cd $REPOSITORY
+REPOSITORY=/home/ubuntu/escapediary-BE-test
+cd $REPOSITORY
 
-# pm2 delete 0
+# pm2 delete 0  #! 대체 이건 왜 안되는 거지??
+pm2 restart all
